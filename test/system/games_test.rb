e093d7eac7d423dd83d470f9a353080f21f72417
Juliana Ariffin <juliana@fastmail.com>
2020-04-14 14:46:17 +0200

    passed tests

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class GamesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit games_url
-  #
-  #   assert_selector "h1", text: "Game"
-  # end
+  test "visiting the index" do
+    visit new_url
+
+    assert_selector "h1", text: "Game"
+  end
 end
